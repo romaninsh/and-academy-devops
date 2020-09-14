19e5216ba933a83558283a88b7803dc256c349a7
Romans Malinovskis <romans.malinovskis@nanoporetech.com>
2020-09-14 14:05:43 +0100

    wip

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+
+variable "env" {
+  default = "dev"
+}
+
+
 resource "aws_s3_bucket" "test" {
-  bucket = "digital.and.romans-test123"
+  bucket = "digital.and.romans-test123-${var.env}"
+  versioning {
+    enabled    = false
+  }
 }
